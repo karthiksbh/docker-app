cc775274c7f4b582e48ca077573edcc8654ce3f1
karthik <karthiksbh@gmail.com>
2024-01-07 23:56:45 -0500

    model changes

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import countModel
 
 # Create your views here.
 def startfunc(request):
-	return HttpResponse("<h1>Home</h1>")
+	return HttpResponse("<h1>Start</h1>");
